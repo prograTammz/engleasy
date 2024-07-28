dcae267aecf680f443f52559ee8d65bf6ea384a5
progratammz <ahmedtamer1998@gmail.com>
2024-07-28 12:38:37 +0300

    added chatbubble styling

--- frontend/src/features/chat-widget/bubbles/ChatBubble.tsx
+++ frontend/src/features/chat-widget/bubbles/ChatBubble.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
 
 interface BotChatBubbleProps {
@@ -11,10 +12,28 @@ interface UserChatBubbleProps {
   text: string;
 }
 
-export const BotChatBubble: React.FC<BotChatBubbleProps> = ({ text }) => {
-  return <div>{text}</div>;
+export const BotChatBubble: React.FC<BotChatBubbleProps> = ({
+  botAvatar,
+  botName,
+  text,
+}) => {
+  return (
+    <div className="flex gap-2 mb-4">
+      <Avatar>
+        <AvatarImage src={botAvatar} alt={botName} />
+        <AvatarFallback>{botName}</AvatarFallback>
+      </Avatar>
+      <div className="bg-gray-200 p-2 rounded-xl rounded-tl-none">{text}</div>
+    </div>
+  );
 };
 
 export const UserChatBubble: React.FC<UserChatBubbleProps> = ({ text }) => {
-  return <div>{text}</div>;
+  return (
+    <div className="flex justify-end gap-2 mb-4">
+      <div className="bg-purple-600 p-2 rounded-xl rounded-tr-none text-white">
+        {text}
+      </div>
+    </div>
+  );
 };
