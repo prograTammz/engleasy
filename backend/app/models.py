0f69031fe56c496313b7cc44159f4ff7d7c99098
progratammz <ahmedtamer1998@gmail.com>
2024-07-29 14:05:05 +0300

    added sender enum to models

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,9 +2,14 @@
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
+from enum import Enum
 
+class SenderType(str, Enum):
+    user = "user"
+    bot = "bot"
 
 class ChatMessage(BaseModel):
     id: Optional[str] = None
     text: str
     timestamp: Optional[datetime] = None
+    sender: SenderType
