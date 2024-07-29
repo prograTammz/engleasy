e521dde9dee10dbbb3503fea6fd6721c211ab36e
progratammz <ahmedtamer1998@gmail.com>
2024-07-29 13:41:40 +0300

    added chat_helper to prevent repeation

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,22 +10,21 @@ database = client.chatbot
 
 chat_collection = database.get_collection("chats")
 
-async def retrieve_chats():
-    chats = []
-    async for chat in chat_collection.find():
-        chats.append({
+def chat_helper(chat) -> dict:
+    return {
         "id": str(chat["_id"]),
         "text": chat["text"],
         "timestamp": chat["timestamp"]
-    })
+    }
+
+async def retrieve_chats():
+    chats = []
+    async for chat in chat_collection.find():
+        chats.append(chat_helper(chat))
     return chats
 
 async def add_chat(chat_data: dict) -> dict:
     chat_data["timestamp"] = datetime.utcnow()
     chat = await chat_collection.insert_one(chat_data)
     new_chat = await chat_collection.find_one({"_id": chat.inserted_id})
-    return {
-        "id": str(new_chat["_id"]),
-        "text": new_chat["text"],
-        "timestamp": new_chat["timestamp"]
-    }
+    return chat_helper(new_chat)
