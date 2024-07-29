1379bbc544097410de04ce27598b0be91145b013
progratammz <ahmedtamer1998@gmail.com>
2024-07-29 13:40:20 +0300

    added get all request

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,18 +2,24 @@
 from fastapi import FastAPI, HTTPException
 from models import ChatMessage
 from database import (
-    add_chat
+    add_chat,
+    retrieve_chats
 )
 from datetime import datetime
 
 app = FastAPI()
 
+@app.get("/chats", response_model=list[ChatMessage])
+async def get_chats():
+    chats = await retrieve_chats()
+    return chats
 
 @app.post("/chats", response_model=ChatMessage)
 async def create_chat(chat: ChatMessage):
     chat.timestamp = datetime.utcnow()
     new_chat = await add_chat(chat.dict())
     return new_chat
+
 
 if __name__ == "__main__":
     import uvicorn
