7d97800488f6999ecef47b92140739bbc5ad8c0f
progratammz <ahmedtamer1998@gmail.com>
2024-07-30 06:50:38 +0300

    added styling to record button

--- frontend/src/features/chat-widget/VoiceRecorder.tsx
+++ frontend/src/features/chat-widget/VoiceRecorder.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+
+interface RecordButtonProps {
+  isRecording: boolean;
+  handleRecording: () => void;
+}
 
 export const VoiceRecorder: React.FC = () => {
-  return <div></div>;
+  const [isRecording, setIsRecording] = useState(false);
+
+  const startRecording = () => {
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+  };
+
+  const handleRecording = () => {
+    if (isRecording) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  };
+
+  return (
+    <div>
+      <RecordButton
+        isRecording={isRecording}
+        handleRecording={handleRecording}
+      />
+    </div>
+  );
+};
+
+const RecordButton: React.FC<RecordButtonProps> = ({
+  isRecording,
+  handleRecording,
+}) => {
+  return (
+    <Button
+      onClick={() => handleRecording()}
+      size="lg"
+      className="animate-pulse rounded-full border-red-500 border-4 p-1 bg-transparent h-12 w-12"
+    >
+      <div className="bg-red-500 w-full h-full rounded-full"></div>
+    </Button>
+  );
 };
