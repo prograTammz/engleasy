68808c37604a5b72603758ab4b95326bcf07fca1
progratammz <ahmedtamer1998@gmail.com>
2024-07-30 10:17:52 +0300

    wrapped app with theme context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "@/contexts/theme";
 import "./styles/global.css";
 import { ChatWindow } from "@/features/chat-widget/ChatWindow";
 
 function App() {
-  return <ChatWindow />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ChatWindow />;
+    </ThemeProvider>
+  );
 }
 
 export default App;
