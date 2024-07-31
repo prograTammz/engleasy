ca4de64b999bbbcd1269f57f3441095f933fe02c
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 04:13:13 +0300

    moved chatMessage to models/chat

---
+++ backend/app/models/chat.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import (Optional)
+from datetime import datetime
+
+
+class ChatMessage(BaseModel):
+    id: Optional[str] = None
+    text: str
+    created: Optional[datetime] = None
+    modified: Optional[datetime] = None
+    is_modified: Optional[bool] = False
+    sender: str = "user"
