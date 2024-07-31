4035be9f330b90a3a89aecd1aec771ec6ad00938
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 05:11:17 +0300

    added init auth service

---
+++ backend/app/services/auth.py
@@ -0,0 +1,9 @@
+from passlib.context import CryptContext
+import os
+
+#Constants for Password hashing and token generation
+SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
