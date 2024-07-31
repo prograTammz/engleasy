1179bcd9bd79e8c30c775b98cbdb3ada1856e18a
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 18:30:32 +0300

    added proper defaults to redis_client

--- backend/app/utils/redis_client.py
+++ backend/app/utils/redis_client.py
@@ -3,7 +3,7 @@ import os
 
 # Redis connection
 redis_client = redis.Redis(
-    host=os.getenv('REDIS_HOST', 'localhost'),
+    host=os.getenv('REDIS_HOST', 'db_redis'),
     port=int(os.getenv('REDIS_PORT', 6379)),
     db=0,
     decode_responses=True
