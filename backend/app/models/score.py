df4b17f28060d1e9eca16c136394e0e9f2609244
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 04:18:33 +0300

    added WritingScores in score model

---
+++ backend/app/models/score.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+class WritingScores(BaseModel):
+    task_achievement: int = Field(..., ge=0, le=20)
+    coherence_and_cohesion: int = Field(..., ge=0, le=20)
+    lexical_resource: int = Field(..., ge=0, le=20)
+    grammatical_range_and_accuracy: int = Field(..., ge=0, le=20)
+    total: int = Field(..., ge=0, le=80)
