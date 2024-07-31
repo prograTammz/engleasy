4843ff182e96622260cd31db6ed10877ceb3bedd
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 04:19:16 +0300

    added SpeakingScores in score Model

--- backend/app/models/score.py
+++ backend/app/models/score.py
@@ -6,3 +6,10 @@ class WritingScores(BaseModel):
     lexical_resource: int = Field(..., ge=0, le=20)
     grammatical_range_and_accuracy: int = Field(..., ge=0, le=20)
     total: int = Field(..., ge=0, le=80)
+
+class SpeakingScores(BaseModel):
+    fluency_and_coherence: int = Field(..., ge=0, le=20)
+    pronunciation: int = Field(..., ge=0, le=20)
+    lexical_resource: int = Field(..., ge=0, le=20)
+    grammatical_range_and_accuracy: int = Field(..., ge=0, le=20)
+    total: int = Field(..., ge=0, le=80)
