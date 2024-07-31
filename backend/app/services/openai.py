85de11bcbd6b09d73f4bed658e656a2c7aeb58c8
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 15:36:36 +0300

    fixes in create_gpt_completion logic

--- backend/app/services/openai.py
+++ backend/app/services/openai.py
@@ -3,12 +3,12 @@ from openai import OpenAI
 client = OpenAI()
 
 # Takes a prompt as a string and creates request to Chatgpt 4o
-async def create_gpt_completion(user_message: str) -> str:
+async def create_gpt_completion(prompt: str) -> str:
 
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
-            {"role": "user", "content": user_message}
+            {"role": "assistant", "content": prompt}
         ]
     )
 
