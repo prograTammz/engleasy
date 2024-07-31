8d01514664baad946b4e61ed5bef63a4ace89fc3
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 04:49:53 +0300

    added redis_client

---
+++ backend/app/utils/redis_client.py
@@ -0,0 +1,10 @@
+import redis
+import os
+
+# Redis connection
+redis_client = redis.Redis(
+    host=os.getenv('REDIS_HOST', 'localhost'),
+    port=int(os.getenv('REDIS_PORT', 6379)),
+    db=0,
+    decode_responses=True
+)
