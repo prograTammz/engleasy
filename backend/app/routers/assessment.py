6b3ea6fc6d311c43fc4047194e664363a0d68001
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 18:28:20 +0300

    added submit_questionaire route

--- backend/app/routers/assessment.py
+++ backend/app/routers/assessment.py
@@ -1,10 +1,11 @@
 import json
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.routers.auth import get_current_user
 from app.models.user import User
 from app.models.assessment import Questionnaire
-from app.services.assessment import generate_questionnaire
+from app.models.score import EnglishScoreSheet
+from app.services.assessment import generate_questionnaire, generate_score_sheet
 from app.utils.redis_client import redis_client
 
 
@@ -20,5 +21,15 @@ async def get_questionnaire(current_user: User = Depends(get_current_user)):
     else:
         questionnaire = Questionnaire.model_validate_json(questionnaire)
     return questionnaire
+
+@router.post("/submit")
+async def submit_questionnaire(questionnaire: Questionnaire, current_user: User = Depends(get_current_user)):
+    user_id = current_user.id
+    # questionnaire_json = redis_client.get(f"questionnaire_{user_id}")
+    # if not questionnaire_json:
+    #     raise HTTPException(status_code=404, detail="Questionnaire not found")
+    score = await generate_score_sheet(questionnaire.model_dump_json(), user_id)
+    redis_client.delete(f"questionnaire_{user_id}")
+    return {"score": score}
 
 
