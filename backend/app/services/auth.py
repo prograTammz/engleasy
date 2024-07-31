d1bc532142263fdd0b7b67046a448b2712306cc9
progratammz <ahmedtamer1998@gmail.com>
2024-07-31 15:57:03 +0300

    added decode_access_token to auth.py

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -23,3 +23,11 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.JWTError:
+        return None
