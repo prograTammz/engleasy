b1cfc3f399c071240803a9e7e3886c855e950659
progratammz <ahmedtamer1998@gmail.com>
2024-08-01 10:01:12 +0300

    added score_id to get_all_scores

--- backend/app/services/scores.py
+++ backend/app/services/scores.py
@@ -11,7 +11,9 @@ async def save_score(score_sheet: EnglishScoreSheet):
 async def get_all_scores(user_id: str) -> list:
     score_sheets = []
     async for score_sheet in scores_collection.find({"user_id": user_id}):
-        score_sheets.append(EnglishScoreSheet.model_validate(score_sheet))
+        score_dict = EnglishScoreSheet.model_validate(score_sheet).model_dump()
+        score_dict['id'] = str(score_sheet['_id'])
+        score_sheets.append(score_dict)
     return score_sheets
 
 # Retrieves single EnglishScore
