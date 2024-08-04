c4861483328c822b08691dde2289ecffe26379a0
progratammz <ahmedtamer1998@gmail.com>
2024-08-04 19:03:39 +0300

    added login page to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import { ThemeProvider } from "@/contexts/theme";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import "@/styles/global.css";
 // Lazy Load the pages
 const Homepage = lazy(() => import("@/pages/Home"));
+const LoginPage = lazy(() => import("@/pages/Login"));
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="dark-mode">
       {/* <ChatWindow />; */}
       <BrowserRouter>
         <Suspense>
           <Routes>
             <Route index element={<Homepage />} />
+            <Route path="login" element={<LoginPage />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
