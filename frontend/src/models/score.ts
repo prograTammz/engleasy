36082f09f27e05441a4ba236e45bb00506141a33
progratammz <ahmedtamer1998@gmail.com>
2024-08-04 10:07:02 +0300

    added properities of Score models

--- frontend/src/models/score.ts
+++ frontend/src/models/score.ts
@@ -1,9 +1,41 @@
-export interface WritingScore {}
+export interface WritingScore {
+  task_achievement: number;
+  coherence_and_cohesion: number;
+  lexical_resource: number;
+  grammatical_range_and_accuracy: number;
+  total: number;
+}
 
-export interface SpeakingScore {}
+export interface SpeakingScore {
+  fluency_and_coherence: number;
+  pronunciation: number;
+  lexical_resource: number;
+  grammatical_range_and_accuracy: number;
+  total: number;
+}
 
-export interface ReadingScore {}
+export interface ReadingScore {
+  understanding_main_ideas: number;
+  understanding_details: number;
+  inference: number;
+  lexical_resource: number;
+  total: number;
+}
 
-export interface ListeningScore {}
+export interface ListeningScore {
+  understanding_main_ideas: number;
+  understanding_details: number;
+  inference: number;
+  lexical_resource: number;
+  total: number;
+}
 
-export interface EnglishScoreSheet {}
+export interface EnglishScoreSheet {
+  user_id: string;
+  test_date: string;
+  writing: WritingScore;
+  speaking: SpeakingScore;
+  reading: ReadingScore;
+  listening: ListeningScore;
+  overall_score: number;
+}
