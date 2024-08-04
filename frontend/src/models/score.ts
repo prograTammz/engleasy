1e39de040d576e0d1cb82b7228daf8cbd1923fe7
progratammz <ahmedtamer1998@gmail.com>
2024-08-04 10:02:23 +0300

    added init model score

---
+++ frontend/src/models/score.ts
@@ -0,0 +1,9 @@
+export interface WritingScore {}
+
+export interface SpeakingScore {}
+
+export interface ReadingScore {}
+
+export interface ListeningScore {}
+
+export interface EnglishScoreSheet {}
