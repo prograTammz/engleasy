ee94c8baba438ff99e14b430f53e70f72866cd9f
progratammz <ahmedtamer1998@gmail.com>
2024-08-05 07:26:38 +0300

    added Rest of the app routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,29 @@ import "@/styles/global.css";
 // Lazy Load the pages
 const Homepage = lazy(() => import("@/pages/Home"));
 const LoginPage = lazy(() => import("@/pages/Login"));
+const RegisterPage = lazy(() => import("@/pages/Register"));
+const AppLayout = lazy(() => import("@/pages/AppLayout"));
+const ScoreSheet = lazy(() => import("@/pages/ScoreSheet"));
+const ScoreSheetList = lazy(() => import("@/pages/ScoreSheetList"));
+const PageNotFound = lazy(() => import("@/pages/PageNotFound"));
+const BotPage = lazy(() => import("@/pages/Bot"));
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="dark-mode">
-      {/* <ChatWindow />; */}
       <BrowserRouter>
         <Suspense>
           <Routes>
-            <Route index element={<Homepage />} />
             <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="home" element={<Homepage />} />
+              <Route path="bot" element={<BotPage />} />
+              <Route path="scores" element={<ScoreSheetList />} />
+              <Route path="scores/:id" element={<ScoreSheet />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
