a0619ea4151ab74194c9368e949d9c31b90b6c09
progratammz <ahmedtamer1998@gmail.com>
2024-08-05 21:09:10 +0300

    added logout to AppLayout

--- frontend/src/pages/AppLayout.tsx
+++ frontend/src/pages/AppLayout.tsx
@@ -8,7 +8,6 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { NavLink, Outlet } from "react-router-dom";
-import { Input } from "@/components/ui/input";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -24,6 +23,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import React from "react";
+import { useAuth } from "@/contexts/auth";
 
 type Route = {
   name: string;
@@ -56,77 +56,83 @@ const NavigationRoutes: React.FC = () => {
 };
 
 const AppLayout: React.FC = () => {
+  const { logout } = useAuth();
+
   return (
-    <div className="grid min-h-screen w-screen md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <NavLink
-              to="home"
-              className="flex items-center gap-2 font-semibold"
-            >
-              <Languages className="h-6 w-6" />
-              <span className="">Engleasy</span>
-            </NavLink>
+    <>
+      <div className="grid min-h-screen w-screen md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <NavLink
+                to="home"
+                className="flex items-center gap-2 font-semibold"
+              >
+                <Languages className="h-6 w-6" />
+                <span className="">Engleasy</span>
+              </NavLink>
+            </div>
+            <div className="flex-1">
+              <NavigationRoutes />
+            </div>
+            <div className="mt-auto p-4"></div>
           </div>
-          <div className="flex-1">
-            <NavigationRoutes />
-          </div>
-          <div className="mt-auto p-4"></div>
+        </div>
+        <div className="flex flex-col">
+          <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <Sheet>
+              <SheetHeader></SheetHeader>
+              <SheetTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="shrink-0 md:hidden"
+                >
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="flex flex-col">
+                <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                  <NavLink
+                    to="home"
+                    className="flex items-center gap-2 font-semibold"
+                  >
+                    <Languages className="h-6 w-6" />
+                    <span className="">Engleasy</span>
+                  </NavLink>
+                </div>
+                <div className="flex-1">
+                  <NavigationRoutes />
+                </div>
+              </SheetContent>
+            </Sheet>
+            <div className="w-full flex-1"></div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="secondary" className="rounded-full">
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => logout()}>
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </header>
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            <Outlet />
+          </main>
         </div>
       </div>
-      <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-          <Sheet>
-            <SheetHeader></SheetHeader>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="flex flex-col">
-              <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                <NavLink
-                  to="home"
-                  className="flex items-center gap-2 font-semibold"
-                >
-                  <Languages className="h-6 w-6" />
-                  <span className="">Engleasy</span>
-                </NavLink>
-              </div>
-              <div className="flex-1">
-                <NavigationRoutes />
-              </div>
-            </SheetContent>
-          </Sheet>
-          <div className="w-full flex-1"></div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="secondary" className="rounded-full">
-                <CircleUser className="h-5 w-5" />
-                <span className="sr-only">Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <Outlet />
-        </main>
-      </div>
-    </div>
+    </>
   );
 };
 
