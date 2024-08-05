e934aa38a2fa3c17e508d78bcff2f36f373679ae
progratammz <ahmedtamer1998@gmail.com>
2024-08-05 21:11:34 +0300

    Fixes in apiAuth: Fixing URLs, Wrong method in login, and added error handling

--- frontend/src/services/apiAuth.ts
+++ frontend/src/services/apiAuth.ts
@@ -4,15 +4,21 @@ const API_URL = "http://localhost:8000/auth";
 
 async function login(user: User): Promise<UserToken> {
   try {
-    const res = await fetch(`${API_URL}/token`, {
+    const res = await fetch(`${API_URL}/login`, {
       body: JSON.stringify(user),
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
     });
+    if (res.status !== 200) {
+      const body = await res.json();
+      throw new Error(body?.detail);
+    }
     return await res.json();
-  } catch {
-    throw new Error("Authentication has failed successfully!");
+  } catch (error: unknown) {
+    const message = (error as Error).message;
+    throw new Error(message ?? "Authentication has failed successfully!");
   }
 }
 
@@ -25,9 +31,14 @@ async function register(user: User): Promise<User> {
         "Content-Type": "application/json",
       },
     });
+    if (res.status !== 201) {
+      const body = await res.json();
+      throw new Error(body?.detail);
+    }
     return await res.json();
-  } catch {
-    throw new Error("Authentication has failed successfully!");
+  } catch (error: unknown) {
+    const message = (error as Error).message;
+    throw new Error(message ?? "Register has failed successfully!");
   }
 }
 
