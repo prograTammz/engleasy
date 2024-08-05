a19ea8b927f19124a3183f6ce7ee0fa817365c98
progratammz <ahmedtamer1998@gmail.com>
2024-08-05 08:11:07 +0300

    added useRecorder custom hook

---
+++ frontend/src/hooks/useRecorder.tsx
@@ -0,0 +1,40 @@
+import { useState, useRef } from "react";
+
+const useRecorder = () => {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
+
+  const startRecording = async () => {
+    setIsRecording(true);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorderRef.current = new MediaRecorder(stream);
+
+    //Push new audio chunks when it's recorded
+    mediaRecorderRef.current.ondataavailable = (event) => {
+      audioChunksRef.current.push(event.data);
+    };
+
+    //Create a blob after recording is finished from the audio chunks
+    mediaRecorderRef.current.onstop = () => {
+      const audioBlob = new Blob(audioChunksRef.current, {
+        type: "audio/mp3",
+      });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudioUrl(audioUrl);
+      audioChunksRef.current = [];
+    };
+
+    mediaRecorderRef.current.start();
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+    mediaRecorderRef.current?.stop();
+  };
+
+  return { isRecording, startRecording, stopRecording, audioUrl };
+};
+
+export default useRecorder;
