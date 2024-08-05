acb4a34b48ab457eecf88c137efbf85968e779d9
progratammz <ahmedtamer1998@gmail.com>
2024-08-05 08:00:20 +0300

    added init useFetch

---
+++ frontend/src/hooks/useFetch.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T,>(url: string, options: object) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
