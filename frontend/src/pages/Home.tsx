c6c17c37c4560ff4750f1d30604e988986ad4505
progratammz <ahmedtamer1998@gmail.com>
2024-08-06 08:38:06 +0300

    added welcome message to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import { useAuth } from "@/contexts/auth";
+
 export default function HomePage() {
+  const { user } = useAuth();
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1>
+        Welcome back,{" "}
+        <span className="text-purple-800 dark:text-purple-400">
+          {user?.name}
+        </span>
+      </h1>
     </main>
   );
 }
