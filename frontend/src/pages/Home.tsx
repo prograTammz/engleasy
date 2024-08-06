6906dd5fdcc3f54d095ca3a8c6dfe26019562cdc
progratammz <ahmedtamer1998@gmail.com>
2024-08-06 09:59:12 +0300

    Added Score component to homePage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,22 @@
+import { Separator } from "@/components/ui/separator";
 import { useAuth } from "@/contexts/auth";
+import { Score } from "@/features/score/Score";
 
 export default function HomePage() {
   const { user } = useAuth();
   return (
-    <main>
+    <>
       <h1>
         Welcome back,{" "}
         <span className="text-purple-800 dark:text-purple-400">
           {user?.name}
         </span>
       </h1>
-    </main>
+      <Separator className="my-6" />
+      <h2 className="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        You latest English Score:
+      </h2>
+      <Score />
+    </>
   );
 }
