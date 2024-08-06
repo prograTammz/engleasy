0b62b0a6b6033124832df7ea93108ad7fc7f483a
progratammz <ahmedtamer1998@gmail.com>
2024-08-06 09:57:19 +0300

    Added ScoreOverview logic

--- frontend/src/features/score/ScoreOverview.tsx
+++ frontend/src/features/score/ScoreOverview.tsx
@@ -1,4 +1,5 @@
 import { Progress } from "@/components/ui/progress";
+import { Separator } from "@/components/ui/separator";
 import { CEFR } from "@/models/score";
 
 type ScoreOverviewProps = {
@@ -16,11 +17,21 @@ export const ScoreOverview: React.FC<ScoreOverviewProps> = ({
   overall_score,
 }) => {
   return (
-    <div className="bg-muted mx-8 my-4">
-      <div>
-        <span>Your English Score</span>
-        <span>{overall_score}</span>
-        <span>{cefr_level}</span>
+    <div className="flex flex-wrap bg-muted mx-8 my-4 justify-center">
+      <div className="flex flex-col gap-2 items-center w-full lg:w-1/5 mb-4 lg:mb-0">
+        <span className="uppercase font-bold">Your English Score</span>
+        <span className="font-bold text-5xl inline-block bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 text-transparent bg-clip-text">
+          {overall_score}
+        </span>
+        <p className="flex font-bold text-lg mb-2 gap-2">
+          Your level is
+          <span className="text-purple-600 dark:text-purple-500">
+            {cefr_level}
+          </span>
+        </p>
+      </div>
+      <Separator orientation="vertical" />
+      <div className="flex items-center w-full lg:w-3/5">
         <ScoreProgress overall_score={overall_score} cefr_level={cefr_level} />
       </div>
     </div>
