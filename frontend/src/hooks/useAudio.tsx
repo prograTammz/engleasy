9b63e88353b9dd1ddc6337834563f1064dd6550d
progratammz <ahmedtamer1998@gmail.com>
2024-08-07 19:48:55 +0300

    added duration and currentTime for progress feature

--- frontend/src/hooks/useAudio.tsx
+++ frontend/src/hooks/useAudio.tsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 
 const useAudio = (url: string) => {
   const [audio, setAudio] = useState<HTMLAudioElement>();
-  const [playing, setPlaying] = useState(false);
+  const [playing, setPlaying] = useState<boolean>(false);
   const [blob, setBlob] = useState<Blob>();
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
 
   const toggle = () => setPlaying(!playing);
 
@@ -19,9 +21,33 @@ const useAudio = (url: string) => {
         const audioUrl = URL.createObjectURL(audioBlob);
         const audio = new Audio(audioUrl);
         setAudio(audio);
+
+        audio.addEventListener("loadedmetadata", () => {
+          setDuration(audio.duration);
+        });
+
+        audio.addEventListener("loadedmetadata", () => {
+          if (audio.duration === Infinity || isNaN(Number(audio.duration))) {
+            audio.currentTime = 1e101;
+            audio.addEventListener("timeupdate", getDuration);
+          }
+        });
+
+        audio.addEventListener("timeupdate", () => {
+          setCurrentTime(audio.currentTime);
+        });
+
+        audio.addEventListener("ended", () => setPlaying(false));
       } catch (error) {
         console.error("Error fetching audio:", error);
       }
+    };
+
+    const getDuration = (event) => {
+      event.target.currentTime = 0;
+      event.target.removeEventListener("timeupdate", getDuration);
+      setDuration(event.target.duration);
+      console.log(event.target.duration, "hi");
     };
 
     fetchAudio();
@@ -44,7 +70,7 @@ const useAudio = (url: string) => {
     }
   }, [audio]);
 
-  return { blob, audio, playing, toggle };
+  return { blob, audio, playing, toggle, currentTime, duration };
 };
 
 export default useAudio;
