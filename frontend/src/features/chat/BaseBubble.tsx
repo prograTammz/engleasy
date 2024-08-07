19fc80a01785a3caaf28c2fbbad6f96eb7cebffe
progratammz <ahmedtamer1998@gmail.com>
2024-08-07 14:52:30 +0300

    added BaseBubble

---
+++ frontend/src/features/chat/BaseBubble.tsx
@@ -0,0 +1,59 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { ChatMessage, ChatSender } from "@/models/chat";
+import React from "react";
+
+type BaseBubbleProps = {
+  chatMessage: ChatMessage;
+  children: React.ReactNode;
+};
+
+export const BaseBubble: React.FC<BaseBubbleProps> = ({
+  chatMessage,
+  children,
+}) => {
+  return (
+    <div
+      className={`flex flex-col ${
+        chatMessage.sender === ChatSender.USER && "items-end"
+      } my-2`}
+    >
+      <div className={`flex gap-2 mb-2`}>
+        {chatMessage.sender === ChatSender.BOT && (
+          <Avatar>
+            <AvatarImage
+              src="https://api.dicebear.com/9.x/micah/svg?seed=Boo"
+              alt="Ava"
+            />
+            <AvatarFallback>Ava</AvatarFallback>
+          </Avatar>
+        )}
+
+        {chatMessage.sender === ChatSender.BOT ? (
+          <div className="bg-gray-200 dark:bg-gray-700 p-3 rounded-3xl rounded-tl-none">
+            {children}
+          </div>
+        ) : (
+          <div className="bg-purple-600 dark:bg-purple-200 p-3 rounded-3xl rounded-tr-none text-white dark:text-black">
+            {children}
+          </div>
+        )}
+      </div>
+      <span className="text-xs text-muted-foreground ">
+        {chatMessage.is_modified
+          ? `Edited at ${formatDate(chatMessage.modified)}`
+          : `Sent at ${formatDate(chatMessage.created)}`}
+      </span>
+    </div>
+  );
+};
+
+function formatDate(date: Date): string {
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  // Time
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+
+  return `${month}/${day}/${year} ${hour}:${minute}`;
+}
