b4d229d7fd1a037ec9ada294e9bbb4ca2a103513
progratammz <ahmedtamer1998@gmail.com>
2024-08-07 15:56:21 +0300

    ScoreListening Component

---
+++ frontend/src/features/score/ScoreListening.tsx
@@ -0,0 +1,170 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { ListeningScore } from "@/models/score";
+import { Label, Pie, PieChart } from "recharts";
+
+type ScoreListeningProps = {
+  listening_score: ListeningScore;
+};
+
+const chartConfig = {
+  score: {
+    label: "Score",
+  },
+  understanding_main_ideas: {
+    label: "Understanding main ideas",
+    color: "hsl(var(--chart-1))",
+  },
+  understanding_details: {
+    label: "Understanding details",
+    color: "hsl(var(--chart-2))",
+  },
+  inference: {
+    label: "Inference",
+    color: "hsl(var(--chart-3))",
+  },
+  lexical_resource: {
+    label: "Lexical Resource",
+    color: "hsl(var(--chart-4))",
+  },
+} satisfies ChartConfig;
+
+export const ScoreListening: React.FC<ScoreListeningProps> = ({
+  listening_score,
+}) => {
+  const chartData = [
+    {
+      skill: "understanding_main_ideas",
+      score: listening_score.understanding_main_ideas,
+      fill: "var(--color-understanding_main_ideas)",
+    },
+    {
+      skill: "understanding_details",
+      score: listening_score.understanding_details,
+      fill: "var(--color-understanding_details)",
+    },
+    {
+      skill: "inference",
+      score: listening_score.inference,
+      fill: "var(--color-inference)",
+    },
+    {
+      skill: "lexical_resource",
+      score: listening_score.lexical_resource,
+      fill: "var(--color-lexical_resource)",
+    },
+  ];
+  return (
+    <div className="flex flex-wrap lg:flex-nowrap gap-4">
+      {/* Score */}
+      <Card className="flex flex-col shadow-md">
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Listening Score</CardTitle>
+          <CardDescription>
+            Breaking down the score based on each skill
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+          >
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={chartData}
+                dataKey="score"
+                nameKey="skill"
+                innerRadius={60}
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          textAnchor="middle"
+                          dominantBaseline="middle"
+                        >
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {listening_score.total.toLocaleString()}
+                          </tspan>
+                          <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                          >
+                            Score
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+      {/* Explanation */}
+      <Card>
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Score Explanation</CardTitle>
+          <CardDescription>
+            Each criterion is scored out of 20, with a total possible score of
+            80 points for Listening.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ul className="flex flex-col gap-2 p-4">
+            <li>
+              <strong className="text-purple-500 dark:text-purple-300">
+                Understanding Main Ideas (0-20 points):
+              </strong>{" "}
+              How well the main ideas are understood.
+            </li>
+            <li>
+              <strong className="text-purple-500 dark:text-purple-300">
+                Understanding Details (0-20 points):
+              </strong>{" "}
+              How well specific details are understood.
+            </li>
+            <li>
+              <strong className="text-purple-500 dark:text-purple-300">
+                Inference (0-20 points):
+              </strong>{" "}
+              The ability to make logical inferences from the listening
+              material.
+            </li>
+            <li>
+              <strong className="text-purple-500 dark:text-purple-300">
+                Lexical Resource (0-20 points):
+              </strong>{" "}
+              Understanding of vocabulary used in the listening material.
+            </li>
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
