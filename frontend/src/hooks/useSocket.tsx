dbd9c9be762fc08ea0646fb0fd2c3ba6785e241c
progratammz <ahmedtamer1998@gmail.com>
2024-08-09 04:48:46 +0300

    fixes in useSocker Hook

--- frontend/src/hooks/useSocket.tsx
+++ frontend/src/hooks/useSocket.tsx
@@ -1,42 +1,55 @@
 import { useEffect, useState } from "react";
 import { UserToken } from "@/models/user";
-import { ChatMessage } from "@/models/chat";
+import { ChatMessage, ChatType } from "@/models/chat";
 
 const useSocket = (url: string, userToken: UserToken) => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [socket, setSocket] = useState<WebSocket | null>();
 
   useEffect(() => {
+    if (socket) {
+      socket.onopen = () => {
+        socket.send(
+          JSON.stringify({ type: "auth", token: userToken.access_token })
+        );
+      };
+
+      socket.onmessage = (event) => {
+        const message: ChatMessage = JSON.parse(JSON.parse(event.data));
+        console.log(message);
+        setMessages((prevMessages) => [...prevMessages, message]);
+      };
+
+      socket.onclose = () => {
+        console.log("WebSocket closed");
+      };
+    }
+
+    // return () => {
+    //   console.log("closed");
+    //   closeSocket();
+    // };
+  }, [url, userToken, socket]);
+
+  const openSocket = () => {
     const socket = new WebSocket(url);
-    socket.onopen = () => {
-      socket.send(
-        JSON.stringify({ type: "auth", token: userToken.access_token })
-      );
-    };
+    setSocket(socket);
+  };
 
-    socket.onmessage = (event) => {
-      const message: ChatMessage = JSON.parse(event.data);
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
-
-    socket.onclose = () => {
-      console.log("WebSocket closed");
-    };
-
-    setWs(socket);
-
-    return () => {
-      socket.close();
-    };
-  }, [url, userToken]);
+  const closeSocket = () => {
+    socket?.close();
+    setSocket(null);
+  };
 
   const sendMessage = (message: ChatMessage) => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.send(JSON.stringify(message));
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      if (message.type === ChatType.TEXT) {
+        socket.send(JSON.stringify(message.content));
+      }
     }
   };
 
-  return { messages, sendMessage };
+  return { messages, openSocket, sendMessage };
 };
 
 export default useSocket;
