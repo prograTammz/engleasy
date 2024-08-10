d1cc5a5f9f0f461d8d5031b40859aaa9e33489e4
progratammz <ahmedtamer1998@gmail.com>
2024-08-10 23:40:06 +0300

    improvements to main.py, origins, host and port is retrieved from .env if provided

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,17 @@
 # main.py
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth, assessment, scores,chat
+from app.routers import auth, assessment, scores, chat
+import os
 
-app = FastAPI()
+app = FastAPI(
+    title="Engleasy Backend",
+    description="AI assistant to improve your English skills",
+    version="0.1.0"
+)
 
-origins = [
-    "http://localhost:4242",  # React frontend URL
-]
+# CORS settings based on environment variables
+origins = os.getenv("CORS_ORIGINS", "http://localhost:4242").split(",")
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,6 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Include routers
 app.include_router(auth.router, prefix="/auth")
 app.include_router(assessment.router, prefix="/assessment")
 app.include_router(scores.router, prefix="/scores")
@@ -24,4 +29,6 @@ app.include_router(chat.router, prefix="/chat")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Host and port can be set via environment variables
+    uvicorn.run(app, host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 8000)))
+
